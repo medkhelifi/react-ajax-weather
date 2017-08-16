a365debf790c6c79c1c1a6a63a1b9f425dbcefee
medkhelifi <mohamedchrif.khelifi@gmail.com>
2017-08-17 00:23:19 +0100

    readme file

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 var Fetch = require('whatwg-fetch');
 var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
-var apiUrl = '&appid=5f4caabf232375d3a3d6b50a978b4b7d';
+var apiUrl = '&appid=YOUR_API_KEY_GOES_HERE';
 
 module.exports = {
     get: function(place) {
